9f5ee7246fd6cae48fcbc7e0ae74934f3c635cd3
Monica <monicams042005@gmail.com>
2026-08-12 14:08:59 +0530

    Add floor and ceiling binary search

---
+++ src/BinarySearch/floor.java
@@ -0,0 +1,29 @@
+package BinarySearch;
+
+public class floor {
+    public static void main(String[] args) {
+         int[] arr = {1,2,3,4,5,6,7,8,9,10,11,15,16,23,24};
+       int target = 13;
+       int ans = floorNum(arr,target);
+       System.out.println(ans);
+    }
+    static int floorNum(int[] arr,int target){
+        int low = 0;
+        int high = arr.length-1;
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(arr[mid]==target){
+                return arr[mid]; 
+            }
+            if(arr[mid]>target){
+                high = mid -1;
+            }else{
+                low = mid +1;
+            }
+        }
+        if(high == -1){
+            return -1;
+        }
+        return arr[high];
+    }
+}
